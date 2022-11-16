df5d8915c71f81e9c3bdf4e8573a61accfb034d1
subbu prasant p <46121063+subbuprasantp@users.noreply.github.com>
2022-11-16 21:20:06 +0530

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cat > backend_generated.tf <<-EOF
-    backend "s3" {
-      bucket = "$1"
-      key    = "$2"
-      region = "$3"
-      dynamodb_table = "$4"
-      encrypt = true  
-    }
+backend "s3" {
+  bucket = "$1"
+  key    = "$2"
+  region = "$3"
+  dynamodb_table = "$4"
+  encrypt = true  
+}
 EOF
 cat backend_generated.tf
