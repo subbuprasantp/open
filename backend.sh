5f84b9ae580e03b3c869baf06e79fb31e4a1f26e
subbu prasant p <46121063+subbuprasantp@users.noreply.github.com>
2022-11-16 18:06:08 +0530

    Add files via upload

---
+++ backend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cat > backend_generated.tf <<-EOF
+  backend "s3" {
+    bucket = $1
+    key    = $2
+    region = $3
+    dynamodb_table = $4
+    encrypt = true  
+  }
+EOF
+sed -i '$d' $5
+cat backend_generated.tf >> $5
+echo "}" >> $5
